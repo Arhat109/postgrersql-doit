da8a6b8216136835808134ffcc248a5e6aa83305
farshatov.v <farshatov.v@wb.ru>
2025-03-23 17:14:25 +0300

    fix

--- HomeWork4/run.sh
+++ HomeWork4/run.sh
@@ -3,7 +3,7 @@
 psql -h 127.0.0.1 -U postgres -W -f create.sql
 
 echo "start term1"
-psql -h 127.0.0.1 -d hw4 -U postgres -W -f term1.sql < password &
+psql -h 127.0.0.1 -d hw4 -U postgres -W -f term1.sql &
 sleep 10
 
 echo "start term2"
